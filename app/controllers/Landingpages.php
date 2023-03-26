6c9e8185026b0ec29af200076c4649a9d9a3f877
Reyhan Gedik <334480@student.mboutrecht.nl>
2023-03-26 23:57:29 +0200

    unittest 1 gemaakt

--- app/controllers/Landingpages.php
+++ app/controllers/Landingpages.php
@@ -5,14 +5,28 @@ class Landingpages extends Controller
 
     public function __construct()
     {
-
     }
 
     public function index()
     {
+        $sumTest = "De som 5 + 2 = " . $this->add(5, 2);
         $data = [
-            'title' => "Homepage Mvc Oop Pdo Toets"
+            'title' => "Homepage Mvc Oop Pdo Toets",
+            'sayHello' => 'Hallo Allemaal',
+            'sumTest' => $sumTest
+
         ];
         $this->view('landingpages/index', $data);
+    }
+
+
+    /**
+    Dit is mijn method afblijven want hij wordt getest 
+     */
+    public function add($number1, $number2)
+    {
+        $sum = $number1 + $number2;
+
+        return $sum;
     }
 }
--- app/views/landingpages/index.php
+++ app/views/landingpages/index.php
@@ -1,2 +1,4 @@
 <h3><?= $data['title'] ?></h3>
+<p><?= $data['sumTest'] ?></p>
+
 <a href="<?= URLROOT . '/Lessen/index'; ?>">Instructeurs in dienst</a>
